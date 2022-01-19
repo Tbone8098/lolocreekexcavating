a347272936edb837ae7e9dc4efaaf9c76a7b3864
tyler thibault <tyler.thibault@protonmail.com>
2022-01-18 17:16:57 -0800

    added side nav

--- lolocreek/components/header/index.jsx
+++ lolocreek/components/header/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import cx from 'classnames'
 import Link from 'next/link'
 
@@ -6,12 +7,23 @@ import StyleCSS from './style.module.css'
 
 // components
 import Button from 'components/button'
+import SideNav from 'components/sideNav'
 
 
 export default function Index() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        let isOpenClone = !isOpen
+        setIsOpen(isOpenClone)
+    }
+
     return (
         <div id='navbar__wrapper'>
-            <div id='navbar__mobile' className='grid md:hidden mx-4'>
+            {
+                isOpen && <SideNav />
+            }
+            <div id='navbar__mobile' className='grid md:hidden mx-4' onClick={toggle}>
                 <div className={cx(StyleCSS.hamburger)}><span></span></div>
             </div>
             <div className={cx('hidden md:grid grid-cols-2 mx-3', StyleCSS.nav)} id='navbar__normal'>
@@ -23,11 +35,11 @@ export default function Index() {
                         <li><Link href="/about">About Us</Link></li>
                         <li><Link href="/services">Services</Link></li>
                         <li className='hidden lg:inline-block'>
-                            <Button 
-                            text="Contact Us"
-                            action={{
-                                url: '/contactus'
-                            }}
+                            <Button
+                                text="Contact Us"
+                                action={{
+                                    url: '/contactus'
+                                }}
                             />
                         </li>
                     </ul>
---
+++ lolocreek/components/sideNav/index.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Link from 'next/link'
+import cx from 'classnames'
+// import {Toggle} from './logic'
+
+// style
+import StyleCSS from './style.module.css'
+import MainCSS from 'styles/main.module.css'
+
+export default function Index() {
+    return (
+        <div className={StyleCSS.sidenav__wrapper}>
+            <div className={StyleCSS.sidenav__container}>
+                <div className={StyleCSS.sidenav__menu}>
+                    <ul className={cx(MainCSS.ul__menu__col)}>
+                        <li><Link href='/about'>About Us</Link></li>
+                        <li><Link href='/services'>Services</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
