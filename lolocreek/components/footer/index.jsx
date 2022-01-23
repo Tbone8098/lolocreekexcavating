a64f9f6f559372646bce83bc8a6fdc045c9b6b4c
tyler thibault <tyler.thibault@protonmail.com>
2022-01-22 20:59:36 -0800

    removed head for real, added alt to image el

--- lolocreek/components/footer/index.jsx
+++ lolocreek/components/footer/index.jsx
@@ -60,6 +60,7 @@ const Index = () => {
                     btnText={
                         <Image
                             src={LogoBlack}
+                            alt='lolo creek excavating logo'
                         />
                     }
                     modal={modalContent()}
