891234a57c2f0977e124fb8cf3c2e75334021747
tyler thibault <tyler.thibault@protonmail.com>
2022-01-26 00:10:11 -0800

    update ' marks

--- lolocreek/pages/about_us/index.jsx
+++ lolocreek/pages/about_us/index.jsx
@@ -33,10 +33,10 @@ export default function Index() {
                                 Lolo Creek Excavating, LLC was started by Brandon Williams in the small town of Lolo, Montana. Brandon has worked in the excavation industry for over 5 years and has experience in a wide variety of construction and excavation applications.
                             </p>
                             <p>
-                                Always ready to take on a challenge, we are looking to target both the residential and commercial markets in the Missoula/Bitterroot Valleys and surrounding areas. We know there is a lack of available excavation companies to work on and complete even the smallest projects. That&apos;'s why our goal at Lolo Creek Excavating is to alleviate those issues by providing prompt and excellent service to our local area home and business owners.
+                                Always ready to take on a challenge, we are looking to target both the residential and commercial markets in the Missoula/Bitterroot Valleys and surrounding areas. We know there is a lack of available excavation companies to work on and complete even the smallest projects. That&apos;s why our goal at Lolo Creek Excavating is to alleviate those issues by providing prompt and excellent service to our local area home and business owners.
                             </p>
                             <p>
-                                As a new company in the area, we are looking to show the industry that we can&apos;'t be beat when it comes to customer service, willingness to go above and beyond, and dedication to making your current and future projects a success.
+                                As a new company in the area, we are looking to show the industry that we can&apos;t be beat when it comes to customer service, willingness to go above and beyond, and dedication to making your current and future projects a success.
                             </p>
                             <p>
                                 Give Brandon a call anytime to chat about your excavation or snow removal needs, and receive a free quote.
