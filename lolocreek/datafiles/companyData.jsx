7fb2fe8dc39629e40f73b2f06bbbd6cd11360f08
tyler thibault <tyler.thibault@protonmail.com>
2022-01-29 17:35:44 -0800

    updated address

--- lolocreek/datafiles/companyData.jsx
+++ lolocreek/datafiles/companyData.jsx
@@ -6,5 +6,5 @@ export const data = {
         cell: '(406) 546-9948'
     },
     email: 'brandon@lolocreekexcavating.com',
-    address: '6940 Mormon Creek Rd. Lolo MT 59847'
+    address: 'Located in Lolo Montana'
 }
