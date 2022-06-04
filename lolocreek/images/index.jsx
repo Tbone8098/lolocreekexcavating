65c4b4688573f65cfeae200295cb2a36a6fec11c
tyler thibault <tyler.thibault@protonmail.com>
2022-06-04 08:55:25 -0700

    init commit branch

--- lolocreek/images/index.jsx
+++
@@ -1,9 +0,0 @@
-import HomepageBG from 'images/homePageBg.jpg';
-import LogoWhite from 'images/logoWhite.png';
-import LogoBlack from 'images/logoBlack.png';
-
-export {
-    HomepageBG,
-    LogoWhite,
-    LogoBlack,
-}
